100b236dec4fb8c93df1a66cd4969d191e7bcecd
mazharul77 <mazharul77.cse@gmail.com>
2021-12-05 23:41:59 +0600

    Area of Equilateral Triangle is programmed

---
+++ equilateralTriangle_Area.py
@@ -0,0 +1,14 @@
+'''
+
+ Problem:
+...... Write a python program to calculate area of an equilateral triangle(সমবাহু ত্রিভুজ) = (math.sqrt(3)/4)*a*a ..........
+ Name: Mazharul Islam Bhuiyan
+ Email:mazharul15-1425@diu.edu.bd
+
+'''
+from math import sqrt
+a = float(input("Enter The Side(for both equal side) of the Triangle:"))
+area_Equil_Tri = ( (sqrt(3)/4) )*a*a
+print("\n\t The Area of Equilateral Triangle is: %.2f" % area_Equil_Tri)
+
+
